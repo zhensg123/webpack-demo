3b3eac55646e376becf9932350c0aec9f12b1e9c
zhensg123 <xyqblog@163.com>
2021-06-20 11:28:02 +0800

    '初始化工程师'

---
+++ Desktop/笔记代码/zujianku/examples/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <ly-link type="primary"> 老严 link </ly-link>
+  </div>
+</template>
+
+<script>
+import lyLink from '../packages/lyLink'
+export default {
+    name: 'App',
+    components: { lyLink }
+}
+</script>
---
+++ Desktop/笔记代码/zujianku/packages/lyLink/src/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <!-- 用传过来 href 进行跳转 --> <!-- 用传过来的 type 修改颜色 --> 
+    <a :href="href || undefined" :class="[`ly-link-${type}`]" >
+        <!-- 使用默认插槽来填充文本 -->
+        <slot/>
+    </a>
+</template>
+
+<script>
+    export default {
+        // 等下 index.js 里面要用到
+        name:"lyLink",
+        props: {
+            // 限制类型
+            href: String,
+            type: {
+                type: String,
+                default: 'default'
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    // 定义链接字体颜色
+    .ly-link-default {
+        color: #606266;
+    }
+    .ly-link-primary {
+        color: #409eff;
+    }
+</style>
